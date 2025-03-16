bf6fe729b81ee3ac4ec76bb481a81be28d1b5ebe
Munkhjin3 <muujii@motforex.com>
2025-03-16 23:42:00 +0800

    fix

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -3,20 +3,23 @@ import { Button, SeatsTableCard } from "@/components";
 import { OrderListTable } from "@/components/dashboard/table-order-list";
 import { Table, useOrderList, useTable } from "@/store/store";
 import { redirect } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const AdminPage = () => {
   const { data, refetch } = useTable();
   const { data: orderList } = useOrderList();
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      redirect("/admin/login");
+    }
+    setIsClient(true);
+  }, []);
 
-  console.log(orderList?.orders);
-
-  const token = localStorage.getItem("token");
-  if (!token) {
-    redirect("/admin/login");
-  }
   const TabText = ["Order list", "Table Seats"];
   const [activeTab, setActiveTab] = useState("Order list");
+  if (!isClient) return null;
   return (
     <div className=" w-full flex flex-col bg-white p-4 h-full">
       <div className="flex gap-2">
