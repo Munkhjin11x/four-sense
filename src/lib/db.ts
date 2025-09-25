3a9a03ecb15115f5d4bb965e3fd2ebcddb67486f
Munkhjin3 <muujii@motforex.com>
2025-09-25 19:10:59 +0800

    remove cloudflare

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,10 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare";
 import { drizzle } from "drizzle-orm/d1";
 import { drizzle as drizzleLocal } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import { cache } from "react";
 import * as schema from "@/db/schema";
 import path from "path";
+import { env } from "process";
 
 export const getDatabase = cache(() => {
   if (process.env.NODE_ENV !== "production") {
@@ -13,11 +13,5 @@ export const getDatabase = cache(() => {
     return drizzleLocal(sqlite, { schema });
   }
 
-  const { env } = getCloudflareContext();
-  return drizzle(env.DB, { schema });
-});
-
-export const getDbAsync = cache(async () => {
-  const { env } = await getCloudflareContext({ async: true });
   return drizzle(env.DB, { schema });
 });
